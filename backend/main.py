dd49a0c016d6b4da82eb70a64ac6f37e7cadf118
Cristian Bicheru <xxthegoldenbiscuitxx@gmail.com>
2019-03-24 02:06:07 -0400

    skeleton

---
+++ backend/main.py
@@ -0,0 +1,147 @@
+import logging
+from flask import Flask, jsonify, request
+import flask_cors
+from google.appengine.ext import ndb
+import google.auth.transport.requests
+import google.oauth2.id_token
+import requests_toolbelt.adapters.appengine
+import calendar
+import time
+
+requests_toolbelt.adapters.appengine.monkeypatch()
+HTTP_REQUEST = google.auth.transport.requests.Request()
+
+app = Flask(__name__)
+flask_cors.CORS(app)
+
+class Account(ndb.Model):
+    email = ndb.StringProperty()
+    expenses = ndb.StringProperty(repeated=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+
+def query_database(user_id):
+    ancestor_key = ndb.Key(Account, user_id)
+    query = Account.query(ancestor=ancestor_key).order(-Account.created)
+    accountData = query.fetch()
+
+    acDataParsed = []
+    if len(accountData) > 0:
+        accountData = accountData[0]
+        acDataParsed.append({
+            'key'       : accountData.key,
+            'expenses'     : accountData.expenses,
+            'created'   : accountData.created
+        })
+
+    return acDataParsed
+
+def stats(data):
+    daily = 0
+    weekly = 0
+    monthly = 0
+    alltime = 0
+    for transaction in data:
+        transaction[2] = float(transaction[2])
+        transaction[0] = float(transaction[0])
+        if time.time()-transaction[2] < 86400:
+            daily += transaction[0]
+            weekly += transaction[0]
+            monthly += transaction[0]
+            alltime += transaction[0]
+        elif time.time()-transaction[2] < 604800:
+            weekly += transaction[0]
+            monthly += transaction[0]
+            alltime += transaction[0]
+        elif time.time()-transaction[2] < 2592000:
+            monthly += transaction[0]
+            alltime += transaction[0]
+        else:
+            alltime += transaction[0]
+    return [daily, weekly, monthly, alltime]
+
+def renderTransactions(data):
+    html = """<thead>
+            <tr>
+              <td class="date">Date</td>
+              <td class="description">Description</td>
+              <td class="amount">Amount</td> 
+            </tr>
+          </thead>\n\n"""
+    for transaction in data:
+        html += '<thead><tr><td class="date">'+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(transaction[2])))+'</td>\n<td class="description">'+str(transaction[1])+'</td>\n<td class="amount">$'+str(transaction[0])+'</td></tr></thead>\n\n\n'
+    return html
+
+@app.route('/accountdata', methods=['GET'])
+def getdata():
+    """Returns a list of notes added by the current Firebase user."""
+
+    # Verify Firebase auth.
+    # [START gae_python_verify_token]
+    id_token = request.headers['Authorization'].split(' ').pop()
+    claims = google.oauth2.id_token.verify_firebase_token(
+        id_token, HTTP_REQUEST)
+    if not claims:
+        return 'Unauthorized', 401
+    # [END gae_python_verify_token]
+
+    data = query_database(claims['sub'])[0]['expenses']
+    data2 = []
+    for i in range(0, len(data), 3):
+        data2.append([data[i], data[i+1], data[i+2]])
+    logging.exception([stats(data2), renderTransactions(data2)])    
+    return jsonify([stats(data2), renderTransactions(data2)])
+
+
+@app.route('/accountdata', methods=['POST', 'PUT'])
+def addexpense():
+
+    # Verify Firebase auth.
+    id_token = request.headers['Authorization'].split(' ').pop()
+    claims = google.oauth2.id_token.verify_firebase_token(
+        id_token, HTTP_REQUEST)
+    if not claims:
+        return 'Unauthorized', 401
+
+    json = request.get_json()
+    data = query_database(claims['sub'])
+
+    if len(data) == 0:
+        return "Unregistered", 99999999999
+    else:
+        data = data[0]
+        acData = Account(
+            parent=ndb.Key(Account, claims['sub']),
+            expenses=data['expenses']+[json['expense1'].replace('$', ''), json['expense2'], str(time.time())])
+
+        # Some providers do not provide one of these so either can be used.
+        acData.email = claims.get('name', claims.get('email', 'Unknown'))
+
+        acData.put()
+    return 'OK', 200
+
+@app.route('/register', methods=['POST', 'PUT'])
+def register():
+
+    # Verify Firebase auth.
+    id_token = request.headers['Authorization'].split(' ').pop()
+    claims = google.oauth2.id_token.verify_firebase_token(
+        id_token, HTTP_REQUEST)
+    if not claims:
+        return 'Unauthorized', 401
+    data = query_database(claims['sub'])
+    if len(data) == 0:
+        acData = Account(
+            parent=ndb.Key(Account, claims['sub']),
+            email = claims.get('name', claims.get('email', 'Unknown')),
+            expenses=[])
+
+        acData.put()
+
+    return 'OK', 200
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
---
+++ frontend/js/main.js
@@ -0,0 +1,157 @@
+$(function(){
+  // This is the host for the backend.
+  // TODO: When running Firenotes locally, set to http://localhost:8081. Before
+  // deploying the application to a live production environment, change to
+  // https://backend-dot-<PROJECT_ID>.appspot.com as specified in the
+  // backend's app.yaml file.
+  //var backendHostUrl = 'https://backend-dot-spent.appspot.com';
+  var backendHostUrl = 'http://localhost:8081';
+
+  // [START gae_python_firenotes_config]
+  // Obtain the following from the "Add Firebase to your web app" dialogue
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBqe0kXeMXXaNaL6J5DtobpqsMK8vW3GM4",
+    authDomain: "spentweb.firebaseapp.com",
+    databaseURL: "https://spentweb.firebaseio.com",
+    projectId: "spentweb",
+    storageBucket: "",
+    messagingSenderId: "714905223411"
+  };
+  // [END gae_python_firenotes_config]
+
+  // This is passed into the backend to authenticate the user.
+  var userIdToken = null;
+
+  // Firebase log-in
+  function configureFirebaseLogin() {
+
+    firebase.initializeApp(config);
+
+    // [START gae_python_state_change]
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        $('#logged-out').hide();
+        var name = user.displayName;
+
+        /* If the provider gives a display name, use the name for the
+        personal welcome message. Otherwise, use the user's email. */
+        var welcomeName = name ? name : user.email;
+
+        user.getToken().then(function(idToken) {
+          userIdToken = idToken;
+
+          /* Now that the user is authenicated, fetch the notes. */
+          register();
+
+          $('#user').text(welcomeName);
+          $('#logged-in').show();
+
+        });
+
+      } else {
+        $('#logged-in').hide();
+        $('#logged-out').show();
+
+      }
+    // [END gae_python_state_change]
+
+    });
+
+  }
+
+  // [START configureFirebaseLoginWidget]
+  // Firebase log-in widget
+  function configureFirebaseLoginWidget() {
+    var uiConfig = {
+      'signInSuccessUrl': '/',
+      'signInOptions': [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url
+      'tosUrl': '<your-tos-url>',
+    };
+
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+  // [END gae_python_firebase_login]
+
+  // [START gae_python_fetch_notes]
+  // Fetch notes from the backend.
+  function getTransactions() {
+    $.ajax(backendHostUrl + '/accountdata', {
+      /* Set header for the XMLHttpRequest to get data from the web server
+      associated with userIdToken */
+      headers: {
+        'Authorization': 'Bearer ' + userIdToken
+      }
+    }).then(function(data){
+      $('#transactionsdiv').empty();
+      $('#transactionsdiv').append(data[1]);
+      $('#amountspentday').empty();
+      $('#amountspentday').append(data[0][0]);
+    });
+  }
+
+  function register() {
+
+    $.ajax(backendHostUrl + '/register', {
+      headers: {
+        'Authorization': 'Bearer ' + userIdToken
+      },
+      method: 'POST',
+      data: JSON.stringify({'empty' : 'header'}),
+      contentType : 'application/json'
+    }).then(function(){
+      getTransactions();
+    });
+  }
+  // [END gae_python_fetch_notes]
+
+  // Sign out a user
+  var signOutBtn =$('#sign-out');
+  signOutBtn.click(function(event) {
+    event.preventDefault();
+
+    firebase.auth().signOut().then(function() {
+      console.log("Sign out successful");
+    }, function(error) {
+      console.log(error);
+    });
+  });
+
+  // Save a note to the backend
+  var saveNoteBtn = $('#add-note');
+  saveNoteBtn.click(function(event) {
+    event.preventDefault();
+
+    var amountField = $('#Amount');
+    var amount = amountField.val();
+    amountField.val("");
+
+    var detailsField = $('#Details');
+    var details = detailsField.val();
+    detailsField.val("");
+
+    /* Send note data to backend, storing in database with existing data
+    associated with userIdToken */
+    $.ajax(backendHostUrl + '/accountdata', {
+      headers: {
+        'Authorization': 'Bearer ' + userIdToken
+      },
+      method: 'POST',
+      data: JSON.stringify({'expense1' : amount, 'expense2' : details}),
+      contentType : 'application/json'
+    }).then(function(){
+      getTransactions();
+    });
+
+  });
+
+  configureFirebaseLogin();
+  configureFirebaseLoginWidget();
+
+});
